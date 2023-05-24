681fcb8bf670229a39f202debb5a7110008a2587
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2023-05-24 21:40:27 +0300

    add inline

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Compliant, Owner
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner
+    raw_id_fields = ['flat']
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
@@ -10,6 +15,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building']
     raw_id_fields = ['like']
+    inlines = [OwnerInline]
 
 
 class CompliantAdmin(admin.ModelAdmin):
