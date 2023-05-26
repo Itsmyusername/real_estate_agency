9536a7851ff38e077df00bb6084b110982a4aa20
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2023-05-26 22:09:21 +0300

    little fix

--- property/migrations/0004_auto_20230517_2236.py
+++ property/migrations/0004_auto_20230517_2236.py
@@ -4,15 +4,15 @@ from django.db import migrations
 
 
 def set_new_building_value(apps, schema_editor):
-    ModelName = apps.get_model('property', 'Flat')
-    ModelName.objects.filter(construction_year__gte=2015).update(new_building=True)
-    ModelName.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 def set_old_building_value(apps, schema_editor):
-    ModelName = apps.get_model('property', 'Flat')
-    ModelName.objects.filter(construction_year__gte=2015).update(new_building=False)
-    ModelName.objects.filter(construction_year__lt=2015).update(new_building=False)
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=False)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 class Migration(migrations.Migration):
