c21f688e8a3a85a7603c1ae239359ef592989de2
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2023-05-26 23:14:21 +0300

    del for

--- property/migrations/0013_auto_20230519_0014.py
+++ property/migrations/0013_auto_20230519_0014.py
@@ -6,13 +6,9 @@ from django.db import migrations
 def fill_class_owner_from_class_flat(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
-    for flat in Flat.objects.all():
-            owner = Owner.objects.get_or_create(
-                owner=flat.owner,
-                owners_phonenumber=flat.owners_phonenumber,
-                owner_pure_phone=flat.owner_pure_phone,
-                flat=flat,
-            )
+    flat_values = Flat.objects.values('owner', 'owners_phonenumber', 'owner_pure_phone', 'flat')
+    owners = [Owner(**flat_value) for flat_value in flat_values]
+    Owner.objects.bulk_create(owners)
 
 
 class Migration(migrations.Migration):
